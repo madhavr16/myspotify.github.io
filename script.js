01129386493995264b924481bc6b587b5483cc2a
madhavr16 <111440555+madhavr16@users.noreply.github.com>
2023-04-15 00:26:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Spotify");
+let songs = [
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+    {songName: 'Let me Love You', filePath: '10.mp3', coverPath: '10.jpg'},
+]
+let audioElement = new Audio('10.mp3');
